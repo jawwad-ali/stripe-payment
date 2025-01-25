41141b8d7c89d6e0344c3741d9e4e2ad8514bf7a
Ali jawwad <33836051+jawwad-ali@users.noreply.github.com>
2025-01-25 20:33:15 +0500

    made ssr false for the component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-import StripePayment from "./components/StripePayment";
+import dynamic from "next/dynamic";
+// import StripePayment from "./components/StripePayment";
+
+const DynamicComponentWithNoSSR = dynamic(
+  () => import('./components/StripePayment'),
+  { ssr: false }
+)
 
 export default function Home() {
   return (
     <main>
-      <StripePayment />
+      <DynamicComponentWithNoSSR />
     </main>
   );
 }
